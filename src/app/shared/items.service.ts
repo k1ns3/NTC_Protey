b38077f7eb57ccaa4a1e616c69cf7a0a89591dc6
k1ns3 <k1ns3za@gmail.com>
2020-08-03 13:27:28 +0300

    [master][add] Подсвечивание маркера на карте, выбранного через лист

--- src/app/shared/items.service.ts
+++ src/app/shared/items.service.ts
@@ -1,20 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ItemsService {
-  public selectedItem: BehaviorSubject<number>;
-
   constructor(private http: HttpClient) {}
 
   public getJSON(): Observable<any> {
     return this.http.get('./assets/data/data.json');
-  }
-
-  public getTaskId(id: BehaviorSubject<number>): BehaviorSubject<number> {
-    return (this.selectedItem = id);
   }
 }
