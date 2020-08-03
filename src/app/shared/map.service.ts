ff4f55ba565d85857f4e2ea0cbdf3a2ad4d2014e
k1ns3 <k1ns3za@gmail.com>
2020-08-03 13:29:31 +0300

    [master][add] Не отслеживаемые изменения к b38077f7eb57ccaa4a1e616c69cf7a0a89591dc6

---
+++ src/app/shared/map.service.ts
@@ -0,0 +1,68 @@
+import { Injectable } from '@angular/core';
+import * as L from 'leaflet';
+import { greenIcon, redIcon } from '../../constants/points';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MapService {
+  public selectedItem: BehaviorSubject<number>;
+  public markers: L.marker[];
+  map: L.Map;
+  oldMarkerId: L.marker;
+  newMarkerId: L.marker;
+  markerId: any;
+
+  constructor() {}
+
+  public onClickMarker(event): void {
+    const layer = event.target;
+    this.map.panTo(layer.getLatLng());
+    this.onChangeItem(event.target);
+  }
+
+  public onChangeItem(layer: L.marker): void {
+    layer.setIcon(redIcon);
+    this.oldMarkerId = this.markers.find(
+      (item) => item.marker.options.id === this.markerId
+    );
+    if (this.oldMarkerId) {
+      this.oldMarkerId.marker.setIcon(greenIcon);
+    }
+    this.markerId = layer.options.id;
+    this.getTaskId(this.markerId);
+  }
+
+  public addObject(): void {
+    this.markers.push({
+      marker: L.marker([59.868909, 30.16526], {
+        icon: greenIcon,
+        id: this.markers.length + 1,
+      })
+        .addTo(this.map)
+        .bindPopup(
+          `<h3 style="color: #264c7c">Мой дом - ${this.markers.length + 1}</h3>`
+        )
+        .on('click', (event) => this.onClickMarker(event)),
+    });
+    console.log(this.markers);
+  }
+
+  public getTaskId(id: BehaviorSubject<number>): BehaviorSubject<number> {
+    this.oldMarkerId = this.markers.find(
+      (item) => item.marker.options.id === id
+    );
+    if (this.oldMarkerId) {
+      this.oldMarkerId.marker.setIcon(redIcon);
+    }
+
+    this.newMarkerId = this.markers.find(
+      (item) => item.marker.options.id === this.selectedItem
+    );
+    if (this.newMarkerId) {
+      this.newMarkerId.marker.setIcon(greenIcon);
+    }
+    return (this.selectedItem = id);
+  }
+}
